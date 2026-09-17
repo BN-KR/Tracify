45c607b9b4b2ae22a4791b90ad85bb5e75f5a4f4
BN-KR <kristoffer.bon@gmail.com>
2026-09-17 23:08:47 +0200

    fix(dashboard): include filter builder dependency

---
+++ src/components/dashboard/filter-builder/index.ts
@@ -0,0 +1,13 @@
+export { FilterBuilder } from "./filter-builder";
+export {
+  type FilterCondition,
+  type FilterColumnDef,
+  type FilterOperator,
+  type FilterValueType,
+  emptyCondition,
+  isConditionActive,
+  operatorsForColumn,
+  defaultOperatorForColumn,
+  OPERATOR_LABEL,
+  OPERATORS_BY_TYPE,
+} from "./types";
